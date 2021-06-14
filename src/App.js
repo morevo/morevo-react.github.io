296286ce52473666e804f445e1408bddb9445b72
Ivan Svilohuzov <svilohuzov.ivan@gmail.com>
2021-06-15 00:29:30 +0300

    Start react-practice Todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/Todolist";
 
 function App() {
+  let tasks = [
+    { id: 1, completed: false, title: "Learn TypeScript" },
+    { id: 2, completed: false, title: "Learn React" },
+    { id: 3, completed: false, title: "Learn JavaScript" },
+    { id: 4, completed: false, title: "Daily to learn some interesting" },
+    { id: 5, completed: false, title: "Get an offer on web. dev." },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__inner">
+        <h1 className="todo__title">Todo list</h1>
+        <TodoList tasks={tasks} />
+      </div>
     </div>
   );
 }
